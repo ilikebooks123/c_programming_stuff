e1eb2bcb774a36ef381a8db9ff1b7857788cf02c
ilikebooks123 <wyue10617@gmail.com>
2024-10-20 23:50:02 -0700

    work

---
+++ 6-lecture-ASCII_print_char_esc-values/homework2.c
@@ -0,0 +1,3 @@
+//this is a trick question and nothing will happen because the code doesnt have a library?
+//or maybe it isnt and if it isnt i think printf("%c\n", c2) will show a u
+//and for printf("%d\n", c2) i think it will show 117
