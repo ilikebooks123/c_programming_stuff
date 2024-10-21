35c36de18939fb376388ed35775be3ebc08ace24
ilikebooks123 <wyue10617@gmail.com>
2024-10-20 23:36:45 -0700

    work

---
+++ 6-lecture-ASCII_print_char_esc-values/homework.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    signed char sc = -10;
+    unsigned char unc = 130;
+    printf("signed char is equal to: %d\n" ,sc);
+    printf("unsigned char is equal to: %d\n" ,unc);
+
+    return 0;
+}
