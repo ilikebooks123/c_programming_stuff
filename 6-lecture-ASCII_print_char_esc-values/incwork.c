155b6c0df0a3d9623406d6f3b9cdec744e71e539
ilikebooks123 <wyue10617@gmail.com>
2024-10-20 23:21:12 -0700

    notes

--- 6-lecture-ASCII_print_char_esc-values/incwork.c
+++ 6-lecture-ASCII_print_char_esc-values/incwork.c
@@ -4,6 +4,7 @@ int main(){
     
     char m = 'M';
     char a = 5;
+    //make sure you are not using character m/'m' or value will change 77->109
     int adding = m + a;
     
     printf("m+a is equal to: %d " ,adding);
