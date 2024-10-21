98f01e7edabe36e2c47aedbe0dbdcc64513f993e
ilikebooks123 <wyue10617@gmail.com>
2024-10-21 00:05:21 -0700

    randomstuff

---
+++ 6-lecture-ASCII_print_char_esc-values/randomthing.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char a = 'A';
+    char b = 1;
+    int result = a + b;
+    printf("a+b is equal to: %d\n" ,result);
+    printf("a+b is equal to: %c\n" ,result);
+
+        return 0;
+}
