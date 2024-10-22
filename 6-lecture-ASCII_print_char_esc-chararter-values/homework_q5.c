2585c4c60e4203dc5eec2d793e7351a5aeee2556
ilikebooks123 <wyue10617@gmail.com>
2024-10-21 20:52:05 -0700

    work

---
+++ 6-lecture-ASCII_print_char_esc-chararter-values/homework_q5.c
@@ -0,0 +1,5 @@
+//maybe this is the trick question because it has no library???
+//or printf("ch1 is %c\n", ch1); will say "ch1 is a"
+//printf("ch2 is %c\n",ch2); will say "ch2 is a"
+//printf("ch3 is %c\n",ch3); will say "ch3 is a"
+//printf("ch1 in decimal: %d\n",ch1) will say "ch1 in decimal:97"
--- 6-lecture-ASCII_print_char_esc-chararter-values/randomthing.c
+++ 6-lecture-ASCII_print_char_esc-chararter-values/randomthing.c
@@ -2,7 +2,7 @@
 
 int main(){
     char a = 'A';
-    char b = 1;
+    char b = '1';
     int result = a + b;
     printf("a+b is equal to: %d\n" ,result);
     printf("a+b is equal to: %c\n" ,result);
