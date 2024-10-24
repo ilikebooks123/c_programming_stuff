36faf08446243b83f18c38c93c345882176756c9
ilikebooks123 <wyue10617@gmail.com>
2024-10-23 19:35:52 -0700

    whoops

--- 6-lecture-ASCII_print_char_esc-chararter-values/homework_q6.c
+++ 6-lecture-ASCII_print_char_esc-chararter-values/homework_q6.c
@@ -1,1 +1,2 @@
-//i think its going to print out 
+//i think printf("c: \test\test.c\n"); will print out "c:     est    est.c  "
+//i think printf("\\test\\01\\test.c\n"); will print out"\test\01\test.c"
