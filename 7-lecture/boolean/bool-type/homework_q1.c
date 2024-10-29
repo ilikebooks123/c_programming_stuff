0e43194880d4d707ae4f8727566b638abc7023be
ilikebooks123 <wyue10617@gmail.com>
2024-10-28 21:31:02 -0700

    work

---
+++ 7-lecture/boolean/bool-type/homework_q1.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(){
+
+    bool isStudent = true;
+    bool isGraduate = false;
+    
+    printf("isStudent is:%d\n", isStudent);
+    printf("isGraduate is:%d", isGraduate);
+
+    return 0;
+
+}
