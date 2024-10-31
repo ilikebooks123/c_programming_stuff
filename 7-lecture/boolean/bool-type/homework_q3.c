52d04ea139d6cbdb73357e12a74f191ece0f4c5d
ilikebooks123 <wyue10617@gmail.com>
2024-10-31 14:46:39 -0700

    test

--- 7-lecture/boolean/bool-type/homework_q3.c
+++ 7-lecture/boolean/bool-type/homework_q3.c
@@ -1,1 +1,5 @@
-//
+//i think printf("%d\n",a1); will print out 1
+//i think printf("%d\n",a2); will print out 1
+//i think printf("%d\n",a3); will print out 1
+//i think printf("%d\n",a4); will print out 1
+//not really suer what printf("%d\n",a5); will print out
---
+++ 7-lecture/boolean/bool-type/test.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main(){
+
+    bool a1 = 5;
+    bool a2 = 10;
+    bool a3 = 15;
+    bool a4 = 20;
+
+    printf("%d\n%d\n%d\n%d\n%d\n", a1, a2, a3, a4);
+
+}
