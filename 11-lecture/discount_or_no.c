691c18f2ee0d273f03dd9ef196ae3a8b29cc5985
ilikebooks123 <wyue10617@gmail.com>
2024-11-29 19:25:44 -0800

    finished

--- 11-lecture/discount_or_no.c
+++ 11-lecture/discount_or_no.c
@@ -1,37 +1,56 @@
 #include <stdio.h>
 
 int main(){
-    int money;
-    char yes;
-    char no;
+    float money;
     char y_n;
     
     printf("Enter money spent:");
-    scanf("%d", &money);
+    scanf("%f", &money);
     
-    printf("Are you a member?(yes/no):");
-    scanf("%f",&y_n);                                                                                                                                                                                                               
-    
-    if(y_n==yes){
-        if(500<=money){
-        printf("Discount:20%%");}
+    printf("Are you a member?(y/n):");
+    scanf(" %c",&y_n);                                                                                                                                                                                                               
+
+
+    if(y_n=='y'){
+        if(500<=money){ 
+            float discount20 = 0.2;
+            printf("money before discount:%f\n", money);
+            float discounted_total20 = money*(1.0-discount20);
+            printf("Discount:20%%\n");
+            printf("Discounted total is:%f", discounted_total20);
+        }
         
         else if(200<=money && money<500){
-        printf("Discount:10%%");
+            float discount10 = .1;
+            printf("money before discount:%f\n", money);
+            float discount_total10 = money*(1.0-discount10);
+            printf("Discount:10%%\n");
+            printf("Discounted total is:%f", discount_total10);
         }
         
         else if(money<200){
-        printf("Discount:5%%");
+            float discount5 = 0.05;
+            printf("money before discount:%f\n", money);
+            float discounted_total5 = money*(1.0-discount5);
+            printf("Discount:5%%\n");
+            printf("Discounted total is:%f", discounted_total5);
         }
     }
     
-    if(y_n==no){
+    if(y_n=='n'){
         if(500<=money){
-        printf("Discount:10%%");
+            float nmdiscount10 = 0.1;
+            printf("money before discount:%f\n", money);
+            float nmdiscounted10_total = money*(1.0-nmdiscount10);
+            printf("Discount:10%%\n");
+            printf("Discounted total is:%f", nmdiscounted10_total);
         }
     
         else if(money<500){
-        printf("Discount:0%%");
+            float nmdiscount;
+            printf("money before discount:%f\n", money);
+            printf("Discount:0%%\n");
+            printf("money after discount:%f", money);
         }  
     }
     
