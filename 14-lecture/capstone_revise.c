4dbd6e25956cc3137498d0839da5007f3915fa66
ilikebooks123 <wyue10617@gmail.com>
2025-01-23 22:20:01 -0800

    completed

--- 14-lecture/capstone_revise.c
+++ 14-lecture/capstone_revise.c
@@ -6,40 +6,64 @@ int main(){
     bool run = true;
     int choice;
     int return_value;
+    int balance = 0;
+    int withdrawal;
+    int deposite;
 
     while (run){
-        printf("enter 1 2 3 or 4\n");
+        printf("\n------------------------------------\n");
+        printf("      Welcome to the simple bank\n");
+        printf("------------------------------------\n");
+        printf("Please select an operation:\n1. Check Balance\n2. Deposite\n3. Withdraw\n4. Exit\nEnter your choice(1-4):");
+        
         return_value = scanf ("%d", &choice);
         
         if (return_value!=1){
-            printf("return_value is not equal to 1\n");
+            printf("you have entered a non-integer character please enter an integer bewteen 1 and 4\n");
         }
         
         else if(choice == 1){
-            printf("choice one successful\n");
+            printf("Your current balance is:%d\n", balance);
         }
 
         else if (choice == 2 ){
-            printf("choice two successful\n");
+            printf("Enter Deposit amount:\n");
+            scanf("%d", &deposite);
+            balance = balance + deposite;
+            printf("Depost successful!\n");
+            printf("Your new balance is:%d\n", balance);
         }
         
         else if (choice == 3){
-            printf("choice three successful\n");
-        }
+            printf("enter withdrawal amount:\n");
+            scanf("%d", &withdrawal);
+            
+            if (balance<withdrawal){
+                printf("Insufficient balance. Withdrawal failed.\n");
+            }
 
+            else if(balance == withdrawal || balance>withdrawal){
+                balance = balance - withdrawal;
+                printf("Withdrawal successful!\n");
+                printf("Your new balance is:%d\n", balance);
+            }
+        }
+        
         else if (choice == 4){
-            printf("choice 4 successful\n");
+            printf("Thank you for using the Simple Bank System. Goodbye!\n");
             run = false;
         }
 
         else{
-            printf("variale not between 1 thorugh 4\n");
+            printf("You have entered an integer greater then 4 or less then 1 please enter a variable between one and four\n");
         }
         
         while (getchar() != '\n');
         
+        
+
     }
-    
+
     return 0;
 
 }
