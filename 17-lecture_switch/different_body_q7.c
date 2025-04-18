41e727dd02d0d63259e85d03514c991477bc2ff7
ilikebooks123 <wyue10617@gmail.com>
2025-04-18 16:17:36 -0700

    q7

--- 17-lecture_switch/different_body_q7.c
+++ 17-lecture_switch/different_body_q7.c
@@ -2,73 +2,44 @@
 
 int main(){
      int BMI;
-     char average;
+     int average;
      int weight;
      float height;
-     int BMIx10;
+     
      printf("Enter weight in kilograms:");
      scanf("%d", &weight);
      printf("Enter height in meters:");
      scanf("%f", &height);
 
      BMI = weight/(height*height);
-     //BMIx10 = BMI*10;
 
-
-
-     // if (BMI < 18.5){
-     //      average = "Underweight";}
+      if (BMI < 18.5){
+           average = 1;}
     
-     // else if (18.5 <= BMI < 24.9){
-     //      average = "Normal weight";}
+      else if (18.5 <= BMI < 24.9){
+           average = 2;}
     
-     // else if (25 <= BMI < 29.9){
-     //      average = "Overweight";}
+      else if (25 <= BMI < 29.9){
+           average = 3;}
     
-     // else{
-     //      average = "Obese";}
+      else{
+           average = 4;}
 
-     switch (BMI){
+     switch (average){
           case 1:
-          case 2:
-          case 3:
-          case 4:
-          case 5:
-          case 6:
-          case 7:
-          case 8:
-          case 9:
-          case 10:
-          case 11:
-          case 12:
-          case 13:
-          case 14:
-          case 15:
-          case 16:
-          case 17:
-          case 18:
                printf("BMI is: %f");
                printf("Underweight");
                break;
-          case 19:
-          case 20:
-          case 21:
-          case 22:
-          case 23:
-          case 24:
-          case 25:
+          case 2:
                printf("BMI is: %f");
                printf("Normal weight");
                break;
-          case 28:
-          case 29:
-          case 30:
+          case 3:
                printf("BMI is: %f");
                printf("Overweight");
                break;
-    
-          default:
-               printf("BMI is: %f");
+          case 4:
+               printf("BMI is: %f\n");
                printf("Obese");
                break;
     }
