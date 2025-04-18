3b2c323c7895ceea82046233e98353471dd7daae
ilikebooks123 <wyue10617@gmail.com>
2025-04-18 16:09:57 -0700

    t

--- 17-lecture_switch/different_body_q7.c
+++ 17-lecture_switch/different_body_q7.c
@@ -4,28 +4,29 @@ int main(){
      int BMI;
      char average;
      int weight;
-     int height;
-
+     float height;
+     int BMIx10;
      printf("Enter weight in kilograms:");
      scanf("%d", &weight);
      printf("Enter height in meters:");
-     scanf("%d", &height);
+     scanf("%f", &height);
 
      BMI = weight/(height*height);
+     //BMIx10 = BMI*10;
 
 
 
-     if (BMI < 18.5){
-          average = "Underweight";}
+     // if (BMI < 18.5){
+     //      average = "Underweight";}
     
-     else if (18.5 <= BMI < 24.9){
-          average = "Normal weight";}
+     // else if (18.5 <= BMI < 24.9){
+     //      average = "Normal weight";}
     
-     else if (25 <= BMI < 29.9){
-          average = "Overweight";}
+     // else if (25 <= BMI < 29.9){
+     //      average = "Overweight";}
     
-     else{
-          average = "Obese";}
+     // else{
+     //      average = "Obese";}
 
      switch (BMI){
           case 1:
@@ -46,6 +47,9 @@ int main(){
           case 16:
           case 17:
           case 18:
+               printf("BMI is: %f");
+               printf("Underweight");
+               break;
           case 19:
           case 20:
           case 21:
@@ -53,15 +57,19 @@ int main(){
           case 23:
           case 24:
           case 25:
+               printf("BMI is: %f");
+               printf("Normal weight");
                break;
           case 28:
           case 29:
           case 30:
-               printf("BMI is: %d", BMI);
-               printf("%d", average);
+               printf("BMI is: %f");
+               printf("Overweight");
                break;
     
           default:
+               printf("BMI is: %f");
+               printf("Obese");
                break;
     }
 
