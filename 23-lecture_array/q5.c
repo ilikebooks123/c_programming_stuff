2617679bf651d670f1216328c0800c44260266c7
ilikebooks123 <wyue10617@gmail.com>
2025-10-12 11:06:47 -0700

    p6_in_progress

--- 23-lecture_array/q5.c
+++ 23-lecture_array/q5.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 int main(){
+    int temp1 = 0;
+    int temp2 = 0;
     int var = 5;
     int arr[6] = {10, 20, 30, 40, 50, 60};
-    for (int i = 0; i < 6; i++){
-        arr[var] = arr[i];
+    for (int i = 0; i < 2; i++){
+        temp1 = arr[i];
+        temp2 = arr[var];
+        arr[i] = temp2;
+        arr[var] = temp1;
         var--;
     }
-    printf("%d\n %d\n %d\n %d\n %d\n %d\n",arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
+    printf("%d\n%d\n%d\n%d\n%d\n%d\n",arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
 
     return 0;
 }
--- 23-lecture_array/q6.c
+++ 23-lecture_array/q6.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 int main(){
-    int arr1[3];
-    int arr2[3];
-    int arr3[3];
+    int arr1[4] = {2, 4, 6, 8};
+    int arr2[4] = {1, 0, 3, 6};
+    int arr3[4];
     
-    for (int i = 0; i < 2; i++){
-        scanf("%d", &arr1[i]);
+    for (int n = 0; n < 4; n++){
+        arr3[n] = arr1[n] + arr2[n]; 
+        printf("%d\n", arr3[n]); 
     }
-    for (int i = 0; i < 2; i++){
-        scanf("%d", &arr2[i]);
-    }
-    for (int n = 0; n < 2; n++){
-        arr1[n] + arr2[n] = arr3[n];
-    }
-
-
+    
 }
+
+    int sum(int arr1[4], int arr2[4]){
+        
+    }
