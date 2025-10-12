265dfbe3f465b6d38fecee7e2d74662e766670bd
ilikebooks123 <wyue10617@gmail.com>
2025-10-12 10:16:08 -0700

    updated_q2

--- 23-lecture_array/q2.c
+++ 23-lecture_array/q2.c
@@ -3,6 +3,11 @@
 int main(){
     int grades[5] = {85, 90, 78, 92, 88};
 
-    printf("My final grade is %d %d %d %d %d this semester.", grades[0], grades[1], grades[2], grades[3], grades[4]);
-
+    //printf("My final grade is %d %d %d %d %d this semester.", grades[0], grades[1], grades[2], grades[3], grades[4]);
+    printf("My final grade is");
+    for (int i = 0; i < 5; i++){
+        printf("%d ", grades[i]);
+    }
+    printf("this semester");
+    
 }
